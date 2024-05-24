c5645052ca37bddbd3f5d29a9b11ac8cfc907637
Nevezhin Yan <yaniknezhin@gmail.com>
2024-05-24 18:17:27 +0500

    Add Solutions

---
+++ SQL_Marathon/Day3/2_finalization/Example_task/180_Consecutive_Numbers/PostgreSQL.sql
@@ -0,0 +1,28 @@
+WITH tmp AS (
+    SELECT
+        num,
+        id,
+        LAG(id) OVER(
+            ORDER BY id
+        ) previd,
+        LAG(num) OVER(
+            ORDER BY id
+        ) prevnum,
+        LEAD(id) OVER(
+            ORDER BY id
+        ) nextid,
+        LEAD(num) OVER(
+            ORDER BY id
+        ) nextnum
+    FROM logs
+)
+
+SELECT
+    DISTINCT num as "ConsecutiveNums" 
+FROM tmp
+WHERE
+    -- ids
+    previd + 1 = id AND id + 1 = nextid
+    AND
+    -- nums
+    prevnum = num AND num = nextnum
